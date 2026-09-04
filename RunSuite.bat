8e84497db1b82c00dc034d124ec8c44949c8aa5f
aadhya-infotech-admin <test1.15july2025@gmail.com>
2026-09-04 14:26:16 +0530

    Latest changes on 4 Sept 2026

--- RunSuite.bat
+++ RunSuite.bat
@@ -1,5 +1,10 @@
 @echo off
-call npx playwright test tests/03-createNewUser.spec.ts
+
+call npx playwright test tests/01-homepageUI.spec.ts tests/02-guestUserTest.spec.ts tests/03-createNewUser.spec.ts --workers=1
+@REM call npx playwright test tests/02-guestUserTest.spec.ts
+@REM call npx playwright test tests/03-createNewUser.spec.ts
+@REM call npx playwright test tests/04-addBalanceTest.spec.ts
+
 @REM call npx playwright test tests --workers=1
 echo Now about to end...
 echo HTML report is generated at ....\playwright-report\index.html
--- tests/04-addBalanceTest.spec.ts
+++ tests/04-addBalanceTest.spec.ts
@@ -22,6 +22,7 @@ let msgText: string;
 let userID: string;
 let password: string;
 let emailAddress: string;
+let balanceAmount: string;
 
 // export class HomePageUItest {
 // constructor(page: Page) {
@@ -66,7 +67,6 @@ test.afterAll(async () => {
   console.log('Teardown: Cleaning up environment...');
   await page.close();
   await context.close();
-  await yopmailContext.close();
   await browser.close();
 });
 
@@ -81,7 +81,7 @@ test.afterAll(async () => {
 //   // await page.waitForFunction(() => document.title.includes('AI Stock Picks') || document.title.includes('Best Stocks to Buy'));
 // });
 
-test('Verify that making deposit of $10 increasing balance $60 in first deposit within 5 minutes', async ({ }, testInfo) => {
+test('Verify that making deposit of $10 by Paypal, increasing balance $60 in first deposit within 5 minutes', async ({ }, testInfo) => {
 
   // Try to read the last created user's details so we can use the email for login
   try {
@@ -101,6 +101,10 @@ test('Verify that making deposit of $10 increasing balance $60 in first deposit 
   await page.click("a:has-text('Claim Now')");
   await page.waitForLoadState('load');
 
+  // await page.locator("div:has-text('PayPal')").click();
+  await page.locator("g[clip-path*='paypal_icon']").click();
+  await page.waitForLoadState('load');
+
   await page.fill("div[class^='input_inputContainer']>input", "10");
 
   await page.locator("button:has-text('Deposit')").scrollIntoViewIfNeeded();
@@ -111,115 +115,30 @@ test('Verify that making deposit of $10 increasing balance $60 in first deposit 
   await page.locator("button:has-text('Confirm Deposit')").click();
   await page.waitForLoadState('load');
 
-
-
-  ///////////============================================
-
-
-
-  let phoneNumber: string;
-
-  userID = 'test' + cfl.getTimestampManual();
-  emailAddress = userID + '@yopmail.com';
-  password = "Test@123";
-  let randomNineDigitNumber = Math.floor(100000000 + Math.random() * 900000000).toString();
-  phoneNumber = '9' + randomNineDigitNumber;
-
-  console.log('Generated User ID:', userID);
-  console.log('Generated Email Address:', emailAddress);
-  console.log('Generated Phone Number:', phoneNumber);
-
-  // await page.goto('https://staging.im-aam.com/');
-  await page.click("button:has-text('Register')");
-  await page.waitForLoadState('load');
-  // await expect(page).toHaveTitle('Register: Start Investing with AI');
-  await page.waitForFunction(() => document.title.includes('Register: Start Investing with AI'));
-  await page.fill("input[placeholder='Full Name']", "TestUser " + userID);
-  await page.fill("input[placeholder='User Name']", userID);
-  await page.fill("input[placeholder='Email']", emailAddress);
-  await page.fill("//input[@type='tel']", "+91" + phoneNumber);
-  await page.fill("input[placeholder='Password']", password);
-  await page.fill("input[placeholder='Confirm Password']", password);
-
-  await page.locator("//input[@type='checkbox']").scrollIntoViewIfNeeded();
-  await page.waitForTimeout(1000); // Wait for 1 second to ensure the checkbox state is updated
-  await page.locator("//input[@type='checkbox']").check();
+  await page.fill("#email']", "bulk-sb-7509c89e59cc4e9f9f079c6ab1@business.example.com");
+  await page.locator("#btnNext").click();
   await page.waitForLoadState('load');
 
-  await page.locator("//button[contains(text(),'Register')]").scrollIntoViewIfNeeded();
-  await page.click("//button[contains(text(),'Register')]");
+  await page.locator("#password").fill("iaam@123");
+
+  await page.locator("#btnLogin").click();
   await page.waitForLoadState('load');
 
-  msgText = await page.locator("div[class*='auth_localPadding']").innerText() || '';
-  console.log('Message Text:', msgText);
-
-  iafl.logIn(emailAddress, "TotallyWrongPassword");
-  msgText = await page.locator("span[class*='auth_formError']").innerText() || '';
-  console.log('Error message text for wrong password before activation: ', msgText);
-  expect.soft(msgText.trim()).toBe('Account is not verified. Please verify your email first.');
-
-  iafl.logIn(userID, password);
+  await page.locator("button:has-text('Complete Purchase')").click();
   await page.waitForLoadState('load');
-  msgText = await page.locator("span[class*='auth_formError']").innerText() || '';
-  console.log('Error message text for non-active account: ', msgText);
-  expect.soft(msgText.trim()).toBe('Account is not verified. Please verify your email first.');
 
-  // await page.goto('https://www.yopmail.com/');
-  // await page.locator("#login").fill(emailAddress);
-  // await page.locator("#login").press('Enter');
-  // await page.waitForLoadState('load');
-  // await page.frameLocator("#ifinbox").locator("div:has-text('Im-Aam')").first().click();
-  // await page.waitForLoadState('load');
+  try {
+    await page.waitForTimeout(6000); // Wait for 6 seconds
+  } catch { }
+  await page.waitForLoadState('load');
 
-  // for (let i = 0; i < 10; i++) {
-  //   try {
-  //     if (await page.locator("//div[contains(text(),'This inbox is empty')]").count() > 0) {
-  //       console.log('Inbox is empty, need to refresh... Attempt #' + (i + 1));
-  //       await page.locator("#refresh").click();
-  //       await page.waitForLoadState('load');
-  //       await page.waitForTimeout(2000);
-  //     } else {
-  //       break;
-  //     }
-  //   } catch (error) {
-  //     console.error('There is exception while checking inbox status:', error);
-  //     page.reload();
-  //     await page.waitForLoadState('load');
-  //     await page.waitForTimeout(2000);
-  //   }
-  // }
+  console.log('Deposit of $10 by Paypal completed. Now verifying the balance increase.');
 
-  // await page.frameLocator("iframe[name='ifinbox']").locator(".lm").click();
+  const rawBalance = await iafl.getAccountBalance();
+  console.log('Balance after deposit: ', rawBalance);
 
-  // const [newPageInNewTab] = await Promise.all([
-  //   page.waitForEvent('popup'),
-  //   page.frameLocator("iframe[name='ifmail']").getByText("Activate Account").click(),
-  // ]);
-
-  // iafl2 = new ImAamFunctionLibrary(newPageInNewTab);
-
-  // let msgTextAfterAccountActivation: string = await newPageInNewTab.locator("div[class^='auth_formContainer']").innerText();
-  // console.log('Message text after clicking on account activation link:', msgTextAfterAccountActivation);
-
-  // if (msgTextAfterAccountActivation === 'Verifying your email...') {
-  //   console.log('Account verification successful message is visible after clicking activation link.');
-  // }
-
-  // expect.soft(msgTextAfterAccountActivation.trim()).toBe('Verifying your email...');
-
-  // await newPageInNewTab.waitForLoadState('load');
-  // await newPageInNewTab.waitForTimeout(8000);
-
-  // msgTextAfterAccountActivation = await newPageInNewTab.locator("div[class*='auth_localPadding']").innerText() || '';
-  // console.log('Message text after few seconds of clicking on account activation link:', msgTextAfterAccountActivation);
-
-  // expect.soft(msgTextAfterAccountActivation.trim()).toBe('Email verified successfully! Login');
-
-  // newPageInNewTab.locator("a:has-text('Login')").click();
-
-  // newPageInNewTab.getByPlaceholder("Enter your username or email").fill(userID);
-  // newPageInNewTab.getByPlaceholder("Enter Password").fill("TotallyWrongPassword");
-  // newPageInNewTab.locator("button:has-text('Login')").click();
+  balanceAmount = rawBalance.includes('$') ? rawBalance.split('$')[1].trim() : rawBalance.trim();
+  console.log('Balance amount after deposit: ', balanceAmount);
 
   if (testInfo.errors.length > 0) {
     console.error("'Create User' Test failed with errors:", testInfo.errors);
@@ -231,169 +150,5 @@ test('Verify that making deposit of $10 increasing balance $60 in first deposit 
 
 });
 
-test('Verify user activation functionality is working fine', async ({ }, testInfo) => {
-  yopmailContext = await browser.newContext({
-    viewport: null,
-    deviceScaleFactor: undefined,
-    isMobile: false,
-    httpCredentials: appHttpCredentials,
-  });
-  const yopmailPage = await yopmailContext.newPage();
 
-  await yopmailPage.goto('https://www.yopmail.com/', { waitUntil: 'domcontentloaded', timeout: 30000 });
-  await yopmailPage.waitForLoadState('load', { timeout: 30000 });
-  await yopmailPage.locator("#login").waitFor({ state: 'visible', timeout: 30000 });
-
-  await yopmailPage.locator("#login").fill(emailAddress);
-  await yopmailPage.locator("#login").press('Enter');
-  await yopmailPage.waitForLoadState('load');
-  // await yopmailPage.frameLocator("#ifinbox").locator("div:has-text('Im-Aam')").first().click();
-
-  for (let i = 0; i < 10; i++) {
-    try {
-      if (await yopmailPage.locator("//div[contains(text(),'This inbox is empty')]").count() > 0) {
-        console.log('Inbox is empty, need to refresh... Attempt #' + (i + 1));
-        await yopmailPage.locator("#refresh").click();
-        await yopmailPage.waitForLoadState('load');
-        await yopmailPage.waitForTimeout(2000);
-      } else {
-        break;
-      }
-    } catch (error) {
-      console.error('There is exception while checking inbox status:', error);
-      yopmailPage.reload();
-      await yopmailPage.waitForLoadState('load');
-      await yopmailPage.waitForTimeout(2000);
-    }
-  }
-
-  await yopmailPage.frameLocator("#ifinbox").locator("div:has-text('IM-AAM')").first().click();
-  await yopmailPage.waitForLoadState('load');
-
-  await yopmailPage.frameLocator("iframe[name='ifinbox']").locator(".lm").click();
-
-  [newPageInNewTab] = await Promise.all([
-    yopmailPage.waitForEvent('popup'),
-    yopmailPage.frameLocator("iframe[name='ifmail']").getByText("Activate Account").click(),
-
-  ]);
-
-  iafl2 = new ImAamFunctionLibrary(newPageInNewTab);
-
-  let msgTextAfterAccountActivation: string = await newPageInNewTab.locator("div[class^='auth_formContainer']").innerText();
-  console.log('Message text after clicking on account activation link:', msgTextAfterAccountActivation);
-
-  if (msgTextAfterAccountActivation === 'Verifying your email...') {
-    console.log('Account verification successful message is visible after clicking activation link.');
-  }
-
-  expect.soft(msgTextAfterAccountActivation.trim()).toBe('Verifying your email...');
-
-  try {
-    await newPageInNewTab.waitForTimeout(8000); // Wait for 8 seconds
-  } catch { }
-  await newPageInNewTab.waitForLoadState('load');
-
-  msgTextAfterAccountActivation = await newPageInNewTab.locator("div[class*='auth_localPadding']").innerText() || '';
-  console.log('Message text after few seconds of clicking on account activation link:', msgTextAfterAccountActivation);
-
-  expect.soft(msgTextAfterAccountActivation.trim()).toBe('Email verified successfully! Login');
-
-  newPageInNewTab.locator("a:has-text('Login')").click();
-  await newPageInNewTab.waitForLoadState('load');
-  // await newPageInNewTab.waitForTimeout(2000);
-  // newPageInNewTab.getByPlaceholder("Enter your username or email").click();
-  await newPageInNewTab.getByPlaceholder("Enter your username or email").fill(userID);
-  newPageInNewTab.getByPlaceholder("Enter Password").fill("TotallyWrongPassword");
-  newPageInNewTab.locator("button:has-text('Login')").click();
-  msgText = await newPageInNewTab.locator("span[class^='auth_formError']").innerText();
-  console.log('Error message text for wrong password after activation: ', msgText);
-  expect.soft(msgText.trim()).toBe('Invalid password.');
-
-  newPageInNewTab.locator("input[type='password']").clear();
-  newPageInNewTab.locator("input[type='password']").fill(password);
-  newPageInNewTab.locator("button:has-text('Login')").click();
-
-  await newPageInNewTab.waitForLoadState('load');
-  await newPageInNewTab.waitForSelector("div[class^='layout_appContainer']", { state: 'attached' });
-  await newPageInNewTab.waitForTimeout(2000);
-
-  if (await newPageInNewTab.locator("div[class^='layout_appContainer']").count() > 0) {
-    console.log('User is able to login successfully after account activation.');
-  } else {
-    console.log('User is NOT able to login successfully after account activation.');
-  }
-  expect.soft(newPageInNewTab.locator("div[class^='layout_appContainer']")).toBeVisible();
-
-  if (testInfo.errors.length > 0) {
-    console.error('Test failed with errors:', testInfo.errors);
-  } else {
-    console.log('Test passed without errors.');
-  }
-
-  // await yopmailContext.close();
-
-});
-
-test('Trial pop-up & balance verifications', async ({ }, testInfo) => {
-  let balanceText: string;
-
-  // expect.soft(page.locator("div[class^='trialpopup_trialPopupContainer']")).toBeVisible();
-  // await expect(newPageInNewTab.locator("div[class^='TrialBanner_bannerContainer']").nth(0)).toBeVisible({ timeout: 10000 });
-  await expect(newPageInNewTab.locator(".singleTable div[class^='TrialBanner_bannerContainer']")).toBeVisible({ timeout: 10000 });
-
-  if ((await newPageInNewTab.locator("button[class^='TrialBanner_closeButton']").nth(0).isVisible()) &&
-    (await newPageInNewTab.locator("div[class^='TrialBanner_infoIcon']").nth(0).isVisible()) &&
-    (await newPageInNewTab.locator("h3:has-text('How Your Free Trial Works')").first().isVisible()) &&
-    // (await newPageInNewTab.locator("text=Your free trial ends in 6 days").first().isVisible()) &&
-    (await newPageInNewTab.locator("button:has-text('Start Your Free Trial Now')").first().isVisible())) {
-    console.log("All elements are present in trial pop-up.");
-    expect.soft(true).toBeTruthy();
-  } else {
-    console.log("All elements are NOT present in trial pop-up.");
-    expect.soft(false).toBeTruthy();
-  }
-
-  await newPageInNewTab.locator("button[class^='TrialBanner_closeButton']").first().click();
-
-  balanceText = await iafl2.getAccountBalance();
-  console.log('Account balance before Top-up: ', balanceText);
-
-  if (testInfo.errors.length > 0) {
-    console.error('Test failed with errors:', testInfo.errors);
-  } else {
-    console.log('Test passed without errors.');
-  }
-
-});
-
-
-test.skip('has expected UI elements', async ({ }, testInfo) => {
-  // test('has expected UI elements', async (testInfo) => {
-  // test('has expected UI elements', async ({page},testInfo) => {
-  // await page.goto('https://staging.im-aam.com/');
-
-  // Check for the presence of key UI elements.
-  await expect.soft(page.locator("img[src='/logo.png']")).toBeVisible();
-  await expect.soft(page.locator("h1:has-text('The Investment Manager Powered by an Artificial Machine')")).toBeVisible();
-  await expect.soft(page.locator("img[src='/assets/landing/landing.png']")).toBeVisible();
-  await expect.soft(page.getByText('Identify the best opportunities in the share market today using AI-powered stock insights')).toBeVisible();
-  await expect.soft(page.locator("button:has-text('Login')")).toBeVisible();
-  await expect.soft(page.locator("button:has-text('Register')")).toBeVisible();
-  await expect.soft(page.locator("p:has-text('Continue As Guest')")).toBeVisible();
-  await expect.soft(page.locator("p[class^='page_landingBottom_text']")).toBeVisible();
-  await expect.soft(page.locator("img[src='/assets/landing/landing.png']")).toBeVisible();
-
-  // if(testInfo.page.pageErrors.length > 0) {
-  //   console.error('Test failed with errors:', testInfo.page.pageErrors);
-  // } else {
-  //   console.log('Test passed without errors.');
-  // }
-
-  if (testInfo.errors.length > 0) {
-    console.error('Test failed with errors:', testInfo.errors);
-  } else {
-    console.log('Test passed without errors.');
-  }
-});
 // }
